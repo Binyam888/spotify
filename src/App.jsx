2289b403d6f8351ea9e9a8d9b1bdaf4580db1530
mohammed binyamin <binyam0068@gmail.com>
2024-02-24 13:50:11 +0530

    added all files

---
+++ src/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className='text-3xl bg-red-900 pl-[100px]'>
+      
+      <h1 className='bg-slate-100'>sdghjgsdjsfdgjsdf</h1>App</div>
+  )
+}
+
+export default App
