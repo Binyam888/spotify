2d75181817eff3b9972a5aaf48602ce5b8aca809
mohammed binyamin <binyam0068@gmail.com>
2024-02-24 17:09:10 +0530

    created index.jsx for styling purpose and better ui

---
+++ src/Index.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import Sidebar from './Components/Sidebar/Sidebar'
+import Hero from './Components/Hero/Hero'
+
+function Index() {
+  return (
+    <div className='Container flex '>
+    
+    <Sidebar/>
+      <Hero/>
+    
+    </div>
+  )
+}
+
+export default Index
